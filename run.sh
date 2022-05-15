9ce4376cc3fe5d9a04294430ecb198cb0ddf750d
yixliu1 <1480169951@qq.com>
2022-05-16 02:38:59 +0800

    bash file

--- run.sh
+++ run.sh
@@ -3,5 +3,17 @@
 sudo apt-get install python3.8
 sudo apt-get install python3-pip
 
-bash fastapi/run.sh
-bash streamlit/run.sh
+#rm pages/backup_page1/data.json
+#rm pages/backup_page2/data.json
+#rm pages/backup_page3/data.json
+
+pip install -r streamlit/requirements.txt
+pip install -r fastapi/requirements.txt
+echo "environment set up finish"
+
+cd fastapi
+python main.py
+
+cd ..
+cd streamlit
+streamlit run main.py
--- streamlit/run.sh
+++ streamlit/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-rm pages/backup_page1/data.json
-rm pages/backup_page2/data.json
-rm pages/backup_page3/data.json
+#rm pages/backup_page1/data.json
+#rm pages/backup_page2/data.json
+#rm pages/backup_page3/data.json
 
 echo "All history data is deleted"
 
