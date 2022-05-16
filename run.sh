e4e8ca14d05eb642e1c8abc77022b2a8e1d83fc0
yixliu1 <1480169951@qq.com>
2022-05-16 23:17:22 +0800

    bug fix

--- run.sh
+++ run.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
-
 sudo apt-get install python3.8
 sudo apt-get install python3-pip
 
-#rm pages/backup_page1/data.json
-#rm pages/backup_page2/data.json
-#rm pages/backup_page3/data.json
-
 pip install -r streamlit/requirements.txt
 pip install -r fastapi/requirements.txt
-echo "environment set up finish"
 
+echo "environment set up finish"
 cd fastapi
 python main.py
 
